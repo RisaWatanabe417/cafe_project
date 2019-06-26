82be958251b19e8b68f239a3127e38a87a70a577
RisaWatanabe417 <aquarius19840122@gmail.com>
2019-06-26 23:15:00 +0900

    環境構築

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = [
+  {
+    entry: {
+			style: [
+				'./src/sass/style.scss',
+			],
+    },
+    devServer: {
+      contentBase: './dist',
+      watchContentBase: true,
+      port: 3000,
+      open: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+						MiniCssExtractPlugin.loader,
+						'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+          use: [
+            {
+              loader: 'url-loader?limit=100000&name=img/[name].[ext]',
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+			new CleanWebpackPlugin(),
+			new MiniCssExtractPlugin({
+				filename: 'css/style.css',
+			}),
+      new CopyWebpackPlugin([
+				{
+					from: path.resolve(__dirname, 'public/'),
+					to: path.resolve(__dirname, 'dist/'),
+        }
+      ]),
+    ],
+  },
+];
